ffb5c6b66a0a51be60250dfe20e9b2812092535d
prachirawat207 <prachirawat207@gmail.com>
2026-08-20 22:32:59 +0530

    Time: 7 ms (9.21%), Space: 15.4 MB (11.9%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+void solve(vector<int>& candidates, int target,vector<vector<int>>&ans,
+        vector<int>temp,int index){
+            if(target==0){
+                ans.push_back(temp);
+                return;
+            }
+            for(int i=index;i<candidates.size();i++){
+                if(candidates[i]>target){
+                    break;
+                }
+                if(i > index && candidates[i]==candidates[i-1]){
+                    continue;
+                }
+                temp.push_back(candidates[i]);
+                solve(candidates,target-candidates[i],ans,temp,i+1);
+                temp.pop_back();
+            }
+}
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        sort(candidates.begin(),candidates.end());
+       solve(candidates,target,ans,temp,0) ;
+       return ans;
+    }
+};
